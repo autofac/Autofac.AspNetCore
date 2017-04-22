c74293588e693e64183e9a38fe5f16e69cab6bc7
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2017-04-22 18:51:48 +1000

    Add project files.

---
+++ src/Autofac.Integration.AspNetCore/WebHostBuilderExtensions.cs
@@ -0,0 +1,49 @@
+﻿// This software is part of the Autofac IoC container
+// Copyright © 2017 Autofac Contributors
+// http://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Autofac.Integration.AspNetCore
+{
+    /// <summary>
+    /// Extension methods for the <see cref="IWebHostBuilder"/> interface.
+    /// </summary>
+    public static class WebHostBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the Autofac <see cref="IServiceProviderFactory{TContainerBuilder}"/> implementation to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="builder">The <see cref="IWebHostBuilder"/> instance being configured.</param>
+        /// <param name="configurationAction">An option action used to configure the container.</param>
+        /// <returns>The existing <see cref="IWebHostBuilder"/> instance.</returns>
+        public static IWebHostBuilder UseAutofac(this IWebHostBuilder builder, Action<ContainerBuilder> configurationAction = null)
+        {
+            return builder.ConfigureServices(services => services.AddAutofac(configurationAction));
+        }
+    }
+}
---
+++ test/Autofac.Integration.AspNetCore.Test/WebHostBuilderExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Autofac.Integration.AspNetCore.Test
+{
+    public class WebHostBuilderExtensionsTests
+    {
+        [Fact]
+        public void UseAutofacAddsFactoryProviderToServiceCollection()
+        {
+            var webHostBuilder = new Mock<IWebHostBuilder>();
+
+            Action<IServiceCollection> serviceAction = null;
+            webHostBuilder.Setup(x => x.ConfigureServices(It.IsAny<Action<IServiceCollection>>()))
+                .Callback<Action<IServiceCollection>>(s => serviceAction = s);
+
+            webHostBuilder.Object.UseAutofac(b => b.RegisterInstance("Foo"));
+
+            var services = new ServiceCollection();
+            serviceAction(services);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var factory = serviceProvider.GetService<IServiceProviderFactory<ContainerBuilder>>();
+
+            Assert.IsType<AutofacServiceProviderFactory>(factory);
+
+            var containerBuilder = factory.CreateBuilder(services);
+            var container = containerBuilder.Build();
+
+            Assert.Equal("Foo", container.Resolve<string>());
+        }
+    }
+}
